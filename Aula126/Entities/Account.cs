f39e56f1c27830afb4c5e0f89d9dfbdd3b579c63
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-05-27 20:32:55 -0300

    first

---
+++ Aula126/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula126.Entities {
+    class Account {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+        //protected permite que subclasses acessem esse atributo;
+
+        public Account() {
+        }
+
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount) {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+    }
+}
---
+++ Aula126/Entities/BusinessAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula126.Entities {
+    class BusinessAccount : Account {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() { 
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+        : base(number,holder,balance) //usa o construtor de Account
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount) {
+            if (amount <= LoanLimit)
+                Balance += amount;
+        }
+    }
+}
