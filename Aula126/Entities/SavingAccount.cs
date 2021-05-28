7ffb3d160f34e9775558bbad479e554c75da8d17
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-05-27 21:27:39 -0300

    second

---
+++ Aula126/Entities/SavingAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula126.Entities {
+    class SavingAccount : Account {
+        public double InterestRate { get; set; }
+
+        public SavingAccount() {
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interstRate) 
+        : base(number, holder, balance){
+            InterestRate = interstRate;
+        }
+
+        public void UpdateBalance() {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- Aula126/Program.cs
+++ Aula126/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using Aula126.Entities;
 
 namespace Aula126 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            BusinessAccount bacc = new BusinessAccount(1001, "Bob", 0.0, 500.00);
+
+            // UPCASTING
+            Account acc = bacc; //um BusinessAccount é essencialmente em Account, então isso é possivel;
+            Account acc1 = new BusinessAccount(1002, "Mary", 0.0, 200.0);
+            Account acc2 = new SavingAccount(1003, "Ana", 0.0, 0.01);
+
+            // DOWNCASTING
+            
+
         }
     }
 }
