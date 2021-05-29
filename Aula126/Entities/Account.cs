401d5a4d19f72d3cc87ca99998819832a10c39f4
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-05-29 19:22:42 -0300

    continuando

--- Aula126/Entities/Account.cs
+++ Aula126/Entities/Account.cs
@@ -19,7 +19,7 @@ namespace Aula126.Entities {
         }
 
         public void Withdraw(double amount) {
-            Balance -= amount;
+            Balance -= amount + 5.00;
         }
 
         public void Deposit(double amount) {
--- Aula126/Program.cs
+++ Aula126/Program.cs
@@ -4,31 +4,16 @@ using Aula126.Entities;
 namespace Aula126 {
     class Program {
         static void Main(string[] args) {
-            BusinessAccount bacc = new BusinessAccount(1001, "Bob", 0.0, 500.00);
+            Account acc1 = new Account(1001, "Alex", 500.00);
+            Account acc2 = new SavingAccount(1002, "Anna", 500.00, 0.01);
 
-            // UPCASTING
-            Account acc = bacc; //um BusinessAccount é essencialmente em Account, então isso é possivel;
-            Account acc1 = new BusinessAccount(1002, "Mary", 0.0, 200.0);
-            Account acc2 = new SavingAccount(1003, "Ana", 0.0, 0.01);
+            acc1.Withdraw(10.00);
+            acc2.Withdraw(10.0);
+            
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
 
-            // DOWNCASTING
-            BusinessAccount bacc2 = (BusinessAccount)acc1;
-            bacc2.Loan(100.00);
-
-            //BusinessAccount bacc3 = (BusinessAccount)acc2; vai dar erro, pois acc2 é do tipo SavingAccount.
-            if (acc2 is BusinessAccount) {
-                //BusinessAccount bacc3 = (BusinessAccount)acc2;
-                BusinessAccount bacc3 = acc2 as BusinessAccount;//Sintaxe Alternativa;
-                bacc3.Loan(200.00);
-                Console.WriteLine("Loan!");
-            }
-            if(acc2 is SavingAccount) {
-                SavingAccount acc3 = (SavingAccount)acc2;
-                //SavingAccount acc3 = acc2 as SavingAccount; -- Sintaxe Alternativa;
-                acc3.UpdateBalance();
-                Console.WriteLine("Update!");
-            }
-
+            Console.ReadKey();
         }
     }
 }
